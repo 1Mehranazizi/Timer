0a12b1472eef589d9ea674a8d5f2964b0d2aa02e
Mehran <info.mehranazizi@yahoo.com>
2022-05-15 03:56:09 -0700

    Set Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SiteFooter = styled.footer`
+    padding:4rem 3rem;
+    background-color:royalblue;
+
+    h3{
+        color:#fff;
+        text-align:center;
+    }
+`
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <SiteFooter>
+            <h3>Footer</h3>
+        </SiteFooter>
     );
 };
 
